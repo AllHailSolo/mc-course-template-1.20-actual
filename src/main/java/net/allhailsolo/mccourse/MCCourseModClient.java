21c062220a78901dd2e5bdca63262859b88becd2
AllHailSolo <theendermight@gmail.com>
2024-01-27 13:20:45 -0500

    added files

---
+++ src/main/java/net/allhailsolo/mccourse/MCCourseModClient.java
@@ -0,0 +1,10 @@
+package net.allhailsolo.mccourse;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MCCourseModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
